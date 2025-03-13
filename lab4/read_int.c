57a8e6ce973981a0b024c92364174b1da76eb8a3
Willy2003-Taylor <willy@Willy.>
2025-03-13 15:29:58 -0600

    Completed lab4

--- lab4/read_int.c
+++ lab4/read_int.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
 
 int readint() {
-  /*
-   * This function takes no parameters, reads in an integer
-   * from the stdin stream (using the getchar function),
-   * and returns that integer. This should skip over any leading
-   * whitespace, and then stop reading the integer the first
-   * time a non-digit character is read.
-   */
+  /*
+   * This function takes no parameters, reads in an integer
+   * from the stdin stream (using the getchar function),
+   * and returns that integer. This should skip over any leading
+   * whitespace, and then stop reading the integer the first
+   * time a non-digit character is read.
+   */
+  int num = 0;
+  char input;
+
+  for(input = getchar(); (input == ' ' || input == '\n' || input == '\r'); input = getchar());
+
+  while(input >= '0' && input <= '9'){
+   num = num * 10 + input - '0';
+   input = getchar();
+  }
+
+  return num;
 }
 
 int main() {
-  // This program reads one integer from the standard input stream
-  // and prints out that integer multiplied by 3!
+  // This program reads one integer from the standard input stream
+  // and prints out that integer multiplied by 3!
   int x = readint();
   printf("%d\n", x*3);
 }
