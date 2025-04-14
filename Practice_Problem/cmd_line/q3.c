4c05d70e6e93662fd7e001a890cd3a107b90f856
Willy Zuo <58131094+Willy2003-Taylor@users.noreply.github.com>
2025-04-13 23:27:03 -0600

    Solution to q3 in Practice Final

--- Practice_Problem/cmd_line/q3.c
+++ Practice_Problem/cmd_line/q3.c
@@ -36,7 +36,6 @@ int** array_input(int n){
 }
 
 void sum_cal(int** arr, int* row_sum, int* column_sum, int* diagonal_sum, int size){
-
     for(int i = 0; i < size; ++i){
         for(int j = 0; j < size; ++j){
             row_sum[i] += arr[i][j];
